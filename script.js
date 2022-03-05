84f812a49ef9b5ca7dca31b9de7cc8c6bea4e943
MrdjaS <mrdjas96@hotmail.com>
2022-03-05 20:29:25 +0100

    Added a project section and improved responsivnes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Hamburger menu//
+
+
 const hamburger = document.querySelector(".hamburger");
 const mainMenu = document.querySelector(".main-menu");
 
@@ -10,6 +13,9 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', 
     hamburger.classList.remove("active");
     mainMenu.classList.remove("active");
 }))
+
+
+//Button to get to top//
 
 
 const mybutton = document.getElementById("myBtn");
@@ -29,4 +35,45 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
+}
+
+//Slider//
+
+const sliderBtn= document.getElementsByClassName("slider-btn");
+const slide = document.getElementById("slide");
+
+sliderBtn[0].onclick = function() {
+  slide.style.transform = "translateX(0px)";
+  for(i=0;i<5;i++) {
+    sliderBtn[i].classList.remove("active")
+  }
+  this.classList.add("active");
+}
+sliderBtn[1].onclick = function() {
+  slide.style.transform = "translateX(-20%)";
+  for(i=0;i<5;i++) {
+    sliderBtn[i].classList.remove("active")
+  }
+  this.classList.add("active");
+}
+sliderBtn[2].onclick = function() {
+  slide.style.transform = "translateX(-40%)";
+  for(i=0;i<5;i++) {
+    sliderBtn[i].classList.remove("active")
+  }
+  this.classList.add("active");
+}
+sliderBtn[3].onclick = function() {
+  slide.style.transform = "translateX(-60%)";
+  for(i=0;i<5;i++) {
+    sliderBtn[i].classList.remove("active")
+  }
+  this.classList.add("active");
+}
+sliderBtn[4].onclick = function() {
+  slide.style.transform = "translateX(-80%)";
+  for(i=0;i<5;i++) {
+    sliderBtn[i].classList.remove("active")
+  }
+  this.classList.add("active");
 }
